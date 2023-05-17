1f70b56a61198e2fcda5f45592fd0cc43818b11b
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-05-17 22:36:50 +0300

    fix (del toastify)

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,22 +2,21 @@ import React, { Component } from 'react';
 import css from './Modal.module.css';
 
 class Modal extends Component {
-
     onEscClick = e => {
         if (e.key === 'Escape') {
-            console.log(e.code)
+            console.log(e.code);
             this.props.onToggle();
         }
-    }
+    };
 
     componentDidMount() {
         window.addEventListener('keydown', this.onEscClick);
     }
 
     componentWillUnmount() {
-        window.removeEventListener('keydown',  this.onEscClick);
+        window.removeEventListener('keydown', this.onEscClick);
     }
-    
+
     render() {
         if (this.props.isOpen) {
             return (
